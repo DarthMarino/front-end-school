7fe226e72fa698e90153dc11a2f1a3d0fe82c031
Marino <marinogomez24@gmail.com>
2020-07-16 02:11:32 -0400

    Added basename instead of history

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createBrowserHistory } from "history";
+import "./index.css";
 
 ReactDOM.render(
-  <BrowserRouter
-    history={createBrowserHistory({ basename: process.env.PUBLIC_URL })}
-  >
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
